2ede1e435cfb3fe1ea9cc649afdc74ab6b2ff717
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2022-03-02 12:47:35 +0900

    認証をやってみた。Localstrageだけど。

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import '../styles/App.css';
 import { CreateLink } from './CreateLink';
 import { Header } from './Header';
 import { LinkList } from './LinkList';
+import { Login } from './Login';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LinkList />} />
           <Route path="/create" element={<CreateLink />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </div>
